5fae3beacae994bd9ddf4b1d231bdfbace30a55b
falloukd9 <fallou102009@gmail.com>
2020-03-05 09:23:01 +0100

    index2.html

---
+++ 03-herhaal.js
@@ -0,0 +1,8 @@
+function herhaal() {
+    var herhaal2 = ['herhaal','<br> herhaal','<br> herhaal','<br> herhaal',
+       '<br> herhaal','<br> herhaal'];
+   
+       for ( var i = 0; i < herhaal2.length; i++) {
+           document.write(herhaal2[i] + ' ' + i );
+       }
+   }
